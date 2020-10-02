62d4e1ac0bdc8f946bcfac952d5e852f4f289f6c
Michael Schantl <michael@schantl-lx.at>
2020-10-02 10:31:28 +0200

    Define units of additional observations

--- bin/user/weatherlink_live/__init__.py
+++ bin/user/weatherlink_live/__init__.py
@@ -61,12 +61,36 @@ _temperature_fields = ["dewpoint2",
                        "thw",
                        "thsw",
                        "inHeatindex"]
+_rain_count_fields = ['rainCount']  # unit: count
+_rain_count_rate_fields = ['rainCountRate']  # unit: count per hour
+_rain_amount_fields = ['rainSize']  # unit: technically rain amount (inch/mm)
 
 schema = {
-    'table': wview_extended.table + [(field, "REAL") for field in _temperature_fields],
-    'day_summaries': wview_extended.day_summaries + [(field, "SCALAR") for field in _temperature_fields]
+    'table': wview_extended.table
+             + [(field, "REAL") for field in _temperature_fields]
+             + [(field, "REAL") for field in _rain_count_fields]
+             + [(field, "REAL") for field in _rain_count_rate_fields]
+             + [(field, "REAL") for field in _rain_amount_fields],
+    'day_summaries': wview_extended.day_summaries
+                     + [(field, "SCALAR") for field in _temperature_fields]
+                     + [(field, "SCALAR") for field in _rain_count_fields]
+                     + [(field, "SCALAR") for field in _rain_count_rate_fields]
+                     + [(field, "SCALAR") for field in _rain_amount_fields]
 }
+
+# Define units of new observation
 weewx.units.obs_group_dict.update(dict([(observation, "group_temperature") for observation in _temperature_fields]))
+weewx.units.obs_group_dict.update(dict([(observation, "group_count") for observation in _rain_count_fields]))
+weewx.units.obs_group_dict.update(dict([(observation, "group_rate") for observation in _rain_count_rate_fields]))
+weewx.units.obs_group_dict.update(dict([(observation, "group_rain") for observation in _rain_amount_fields]))
+
+# Define unit group 'group_rate'
+weewx.units.USUnits['group_rate'] = 'per_hour'
+weewx.units.MetricUnits['group_rate'] = 'per_hour'
+weewx.units.MetricWXUnits['group_rate'] = 'per_hour'
+
+weewx.units.default_unit_format_dict['per_hour'] = '%.0f'
+weewx.units.default_unit_label_dict['per_hour'] = ' per hour'
 
 
 def loader(config_dict, engine):
