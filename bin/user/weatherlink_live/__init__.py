4289675da43bdabc30f1d0cbec8b5b164b3a2739
Michael Schantl <michael@schantl-lx.at>
2020-10-03 19:32:40 +0200

    Bump version to v1.0.2

--- bin/user/weatherlink_live/__init__.py
+++ bin/user/weatherlink_live/__init__.py
@@ -33,7 +33,7 @@ from weewx.drivers import AbstractDevice
 from weewx.engine import InitializationError
 
 DRIVER_NAME = "WeatherLinkLive"
-DRIVER_VERSION = "1.0.1"
+DRIVER_VERSION = "1.0.2"
 
 log = logging.getLogger(__name__)
 
