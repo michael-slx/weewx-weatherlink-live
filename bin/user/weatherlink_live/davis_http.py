6bc2340a4ae9bf062fe35fb741d649a7f7d576ef
Michael Schantl <michael@schantl-lx.at>
2020-11-20 09:01:07 +0100

    Retry failed HTTP requests

--- bin/user/weatherlink_live/davis_http.py
+++ bin/user/weatherlink_live/davis_http.py
@@ -17,19 +17,53 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+import logging
+import time
+from typing import Optional
 
 import requests
 
 from user.weatherlink_live.packets import WlHttpBroadcastStartRequestPacket, WlHttpConditionsRequestPacket
+from weewx import WeeWxIOError
+
+log = logging.getLogger(__name__)
 
 
 def start_broadcast(host: str, duration):
-    r = requests.get("http://%s:80/v1/real_time?duration=%d" % (host, duration))
-    json = r.json()
-    return WlHttpBroadcastStartRequestPacket.try_create(json, host)
+    error: Optional[Exception] = None
+
+    for i in range(3):
+        try:
+            r = requests.get("http://%s:80/v1/real_time?duration=%d" % (host, duration))
+            json = r.json()
+            return WlHttpBroadcastStartRequestPacket.try_create(json, host)
+        except Exception as e:
+            error = e
+            log.error(e)
+            log.error("HTTP Request failed. Retry #%d follows shortly" % i)
+        time.sleep(2.5)
+
+    if error is not None:
+        raise error
+
+    raise WeeWxIOError("HTTP request failed without setting an error")
 
 
 def request_current(host: str):
-    r = requests.get("http://%s:80/v1/current_conditions" % host)
-    json = r.json()
-    return WlHttpConditionsRequestPacket.try_create(json, host)
+    error: Optional[Exception] = None
+
+    for i in range(3):
+        try:
+            r = requests.get("http://%s:80/v1/current_conditions" % host)
+            json = r.json()
+            return WlHttpConditionsRequestPacket.try_create(json, host)
+        except Exception as e:
+            error = e
+            log.error(e)
+            log.error("HTTP Request failed. Retry #%d follows shortly" % i)
+        time.sleep(2.5)
+
+    if error is not None:
+        raise error
+
+    raise WeeWxIOError("HTTP request failed without setting an error")
