dac5a2952b50d434bda6deca541b3a53044989af
Michael Schantl <michael@schantl-lx.at>
2021-04-16 15:24:16 +0200

    Fix test install script
    Tmp directory has to be created before install

--- testing/bin/install-wll-drv.sh
+++ testing/bin/install-wll-drv.sh
@@ -3,6 +3,7 @@ set -e
 
 drv_dir="/tmp/weewx-weatherlinklive-driver"
 
+[[ -d "$drv_dir" ]] || sudo mkdir -p "$drv_dir"
 sudo cp -r /vagrant/* $drv_dir
 sudo wee_extension --install=$drv_dir /etc/weewx/weewx.conf
 sudo rm -fr $drv_dir
