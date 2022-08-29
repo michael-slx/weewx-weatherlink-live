163d2448e6c9d5dff13ac839bb7122a94b9de5fd
Michael Schantl <floss@schantl-lx.at>
2022-08-29 18:21:06 +0200

    Switch testing VM to provision with YAY

--- testing/bin/provision.sh
+++ testing/bin/provision.sh
@@ -3,27 +3,7 @@
 set -e
 
 sudo hostnamectl set-hostname "vm-weewx-wll-test"
-sudo pacman -Syyuu --noconfirm
-
-if ! pacman -Qe | cut -f 1 -d " " | grep -q "python-requests"; then
-  sudo pacman -S --noconfirm base-devel git wget curl freetype2 python-requests python-objgraph
-fi
-
-[[ -d "$HOME/aur/mslx" ]] || git clone https://github.com/michael-slx/aur-packages.git "$HOME/aur/mslx"
-[[ -d "$HOME/aur/python-cheetah3" ]] || git clone https://aur.archlinux.org/python-cheetah3.git "$HOME/aur/python-cheetah3"
-[[ -d "$HOME/aur/python-pyephem" ]] || git clone https://aur.archlinux.org/python-pyephem.git "$HOME/aur/python-pyephem"
-
-cd "$HOME/aur/python-cheetah3"
-git pull
-makepkg -si --noconfirm
-
-cd "$HOME/aur/python-pyephem"
-git pull
-makepkg -si --noconfirm
-
-cd "$HOME/aur/mslx/weewx"
-git pull
-makepkg -si --noconfirm
+yay -Syyuu --noconfirm weewx python-requests
 
 sudo cp -fv /vagrant/testing/conf/weewx.conf /etc/weewx/weewx.conf
 
