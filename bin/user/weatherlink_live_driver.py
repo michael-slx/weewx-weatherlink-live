3831c237932b9addb9e9a3f28317bed82acf2111
Michael Schantl <floss@schantl-lx.at>
2023-03-17 15:36:59 +0100

    Add alias for user.weatherlink_live in root module

---
+++ bin/user/weatherlink_live_driver.py
@@ -0,0 +1,10 @@
+from user import weatherlink_live
+
+DRIVER_NAME = weatherlink_live.DRIVER_NAME
+DRIVER_VERSION = weatherlink_live.DRIVER_VERSION
+
+schema = weatherlink_live.schema
+
+loader = weatherlink_live.loader
+confeditor_loader = weatherlink_live.confeditor_loader
+configurator_loader = weatherlink_live.configurator_loader
