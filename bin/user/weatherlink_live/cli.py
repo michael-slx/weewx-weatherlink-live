74364ea546bd113c137b468865ba76eca0a4165d
Michael Schantl <floss@schantl-lx.at>
2023-04-16 13:13:10 +0200

    Add constants for colorizing terminal output

--- bin/user/weatherlink_live/cli.py
+++ bin/user/weatherlink_live/cli.py
@@ -62,3 +62,12 @@ def prompt_int_range(prompt: str, min_value: int, max_value: int, default_value:
         raise AbortCliMenu() from e
     except KeyboardInterrupt as e:
         raise AbortCliMenu() from e
+
+
+class Colors:
+    HEADER = '\033[36m'
+    OK = '\033[92m'
+    FAIL = '\033[91m'
+    BOLD = '\033[1m'
+    STANDOUT = '\033[7m'
+    END = '\033[0m'
