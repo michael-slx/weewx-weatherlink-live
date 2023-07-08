afeb273736d66334869063c4232a7ea68e4e13d5
Michael Schantl <floss@schantl-lx.at>
2023-07-08 14:24:51 +0200

    Bump version to 1.1.1

--- install.py
+++ install.py
@@ -33,7 +33,7 @@ class WeatherLinkLiveInstaller(ExtensionInstaller):
     def __init__(self):
         super(WeatherLinkLiveInstaller, self).__init__(
             name='weatherlink-live',
-            version="1.1.0",
+            version="1.1.1",
             description='WeeWX driver for Davis WeatherLink Live.',
             author="Michael Schantl",
             author_email="floss@schantl-lx.at",
